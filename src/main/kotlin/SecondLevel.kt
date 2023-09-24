682411148c52b778889610c072e67f1bd3dd3286
Hnatiuk Serhiy <hnatiuk.fw@gmail.com>
2023-09-24 17:42:13 +0300

    Implement second level

---
+++ src/main/kotlin/SecondLevel.kt
@@ -0,0 +1,9 @@
+class SecondLevel {
+    fun getCountOfThreeDigitNumbers(numbers: Array<Int>): Int {
+        return numbers.filter { num -> isThreeDigitNumber(num) }.size
+    }
+
+    private fun isThreeDigitNumber(number: Number): Boolean {
+        return number.toString().length == 3
+    }
+}
