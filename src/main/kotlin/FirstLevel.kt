30721c7c24dc584a93537f0d9d3e0360e8ff041e
Hnatiuk Serhiy <hnatiuk.fw@gmail.com>
2023-09-24 17:40:53 +0300

    Implement first level

---
+++ src/main/kotlin/FirstLevel.kt
@@ -0,0 +1,26 @@
+import kotlin.math.cos
+import kotlin.math.sin
+
+class FirstLevel {
+    fun getAngleInfo(angleInDegrees: Double): String {
+        val trigonometricInfo = getTrigonometricFunctionsInfo(angleInDegrees)
+        val angleInRadians = getAngleInRadians(angleInDegrees)
+
+        return "$trigonometricInfo. Value in radians: $angleInRadians."
+    }
+
+    private fun getTrigonometricFunctionsInfo(angleInDegrees: Double): String {
+        val angleSin = sin(angleInDegrees)
+        val angleCos = cos(angleInDegrees)
+
+        return if (angleSin > angleCos) {
+            "Angle sin is greater"
+        } else {
+            "Angle cos is greater"
+        }
+    }
+
+    private fun getAngleInRadians(angleInDegrees: Double): Double {
+        return angleInDegrees * (Math.PI / 180)
+    }
+}
